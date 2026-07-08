1614b523232123660240d6592743bb29a10f7713
Yidnekachew-SK <yidnekachewsk@gmail.com>
2026-07-08 18:33:53 +0300

    fix spelling in import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 import Controller from "./Components/controller.jsx";
-import HomePage from "./Components/homePage.jsx";
+import HomePage from "./Components/homepage.jsx";
 import ShopPage from "./Components/shopPage.jsx";
 import CartPage from "./Components/cartPage.jsx";
 
